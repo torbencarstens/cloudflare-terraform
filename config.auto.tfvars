dd954af45c8c6bebf04a286b1735bc2390f70d22
Torben Carstens <github@carstens.tech>
2021-04-11 18:45:01 +0200

    Update dns records

--- config.auto.tfvars
+++ config.auto.tfvars
@@ -1,3 +1,3 @@
-proxy_home_ip = true
-proxy_home_cnames = true
+proxy_home_ip = false
+proxy_home_cnames = false
 home_ip = "31.165.10.136"
--- config.tf
+++ config.tf
@@ -52,9 +52,89 @@ variable "proxy_home_cnames" {
   type    = bool
 }
 
+resource "cloudflare_record" "home_domain_a" {
+  zone_id = var.zone_id
+  name    = var.home_domain
+  type    = "A"
+  value   = var.home_ip
+  proxied = var.proxy_home_ip
+}
+
+resource "cloudflare_record" "{{rep}}_cname" {
+  zone_id = var.zone_id
+  name    = "{{rep}}"
+  type    = "CNAME"
+  value   = var.home_domain
+  proxied = var.proxy_home_cnames
+}
+
+resource "cloudflare_record" "vpn_cname" {
+  zone_id = var.zone_id
+  name    = "vpn"
+  type    = "CNAME"
+  value   = var.home_domain
+  proxied = var.proxy_home_cnames
+}
+
+resource "cloudflare_record" "synclounge_cname" {
+  zone_id = var.zone_id
+  name    = "synclounge"
+  type    = "CNAME"
+  value   = var.home_domain
+  proxied = var.proxy_home_cnames
+}
+
+resource "cloudflare_record" "share_cname" {
+  zone_id = var.zone_id
+  name    = "share"
+  type    = "CNAME"
+  value   = var.home_domain
+  proxied = var.proxy_home_cnames
+}
+
+resource "cloudflare_record" "rss_cname" {
+  zone_id = var.zone_id
+  name    = "rss"
+  type    = "CNAME"
+  value   = var.home_domain
+  proxied = var.proxy_home_cnames
+}
+
+resource "cloudflare_record" "plex_cname" {
+  zone_id = var.zone_id
+  name    = "plex"
+  type    = "CNAME"
+  value   = var.home_domain
+  proxied = var.proxy_home_cnames
+}
+
+resource "cloudflare_record" "pihole_cname" {
+  zone_id = var.zone_id
+  name    = "pihole"
+  type    = "CNAME"
+  value   = var.home_domain
+  proxied = var.proxy_home_cnames
+}
+
+resource "cloudflare_record" "grocy_cname" {
+  zone_id = var.zone_id
+  name    = "grocy"
+  type    = "CNAME"
+  value   = var.home_domain
+  proxied = var.proxy_home_cnames
+}
+
 resource "cloudflare_record" "grafana_cname" {
   zone_id = var.zone_id
   name    = "grafana"
+  type    = "CNAME"
+  value   = var.home_domain
+  proxied = var.proxy_home_cnames
+}
+
+resource "cloudflare_record" "ddns_cname" {
+  zone_id = var.zone_id
+  name    = "ddns"
   type    = "CNAME"
   value   = var.home_domain
   proxied = var.proxy_home_cnames
@@ -66,13 +146,4 @@ resource "cloudflare_record" "hoas_cname" {
   type    = "CNAME"
   value   = var.home_domain
   proxied = var.proxy_home_cnames
-}
-
-
-resource "cloudflare_record" "home_domain_a" {
-  zone_id = var.zone_id
-  name    = var.home_domain
-  type    = "A"
-  value   = var.home_ip
-  proxied = var.proxy_home_ip
 }
