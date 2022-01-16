0de8704ca906b40c58f85035092340d3630ff00a
Torben Carstens <git@carstens.tech>
2022-01-16 17:07:17 +0100

    Run `terraform fmt`

--- vars.tf
+++ vars.tf
@@ -14,47 +14,47 @@ variable "home_domain" {
 }
 
 variable "kubernetes_domain" {
-  type = string
+  type    = string
   default = "k8s.carstens.tech"
 }
 
 variable "zone_id" {
-  type    = string
+  type = string
 }
 
 variable "proxy_home_ip" {
-    type = bool
-    default = false
+  type    = bool
+  default = false
 }
 
 variable "proxy_home_cnames" {
-    type = bool
-    default = false
+  type    = bool
+  default = false
 }
 
 variable "proxy_kubernetes_ip" {
-    type = bool
-    default = false
+  type    = bool
+  default = false
 }
 
 variable "proxy_kubernetes_cname" {
-    type = bool
-    default = false
+  type    = bool
+  default = false
 }
 
 variable "home_ip" {
-    type = string
+  type = string
 }
 
 variable "local_dns_ip" {
-    type = string
-    default = "10.10.10.1"
+  type    = string
+  default = "10.10.10.1"
 }
 
 variable "kubernetes_ip" {
-    type = string
+  type = string
 }
 
 variable "kubernetes_ipv6" {
-    type = string
+  type = string
 }
